db228878c2dcc4a645d23968cc3a2a45fb3e9abf
AngSauce <ingangelsaucedo@gmail.com>
2026-03-07 20:05:46 -0600

    Configuracio de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://angsauce.github.io',
+  site: 'https://angsauce.github.io/',
   base: '/PortafolioWeb/',
   vite: {
     plugins: [tailwindcss()]
